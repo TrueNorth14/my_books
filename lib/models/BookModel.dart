49dbae161909cc805dce65b3d39f8055e770a1bf
Jungwoo <jwoooh0719@gmail.com>
2019-12-22 15:41:28 -0500

    Getting rid of useless Constructors/getters in BookModel.dart

--- lib/models/BookModel.dart
+++ lib/models/BookModel.dart
@@ -9,7 +9,6 @@ class BookModel {
 
   /// Instance fields that gets fetched automatically
   String coverURL;
-  //String genre; genre not available from api
   String description;
   String publisher;
 
@@ -37,34 +36,6 @@ class BookModel {
     this._publishedDay,
     this._publishedYear,
   );
-
-  /// Named constructor for initiating with ISBN number
-  BookModel.withISBN(this.isbn) {
-    initializeWithISBN();
-  }
-
-  /* 
-       * Dummy method for initializing instance variables with ISBN.
-       * To implement this, get data(online) about book using ISBN (preferrably
-         another class/method) then simply initialize the instance fields.
-       * This method gets called in the withISBN() named constructor.
-    */
-  dynamic initializeWithISBN() {
-    // TODO: Implement this method
-  }
-
-    Map<String,dynamic> get map {
-    return {
-      "isbn": this.isbn,
-      "title": this.title,
-      "author": this.author,
-      "publishedMonth": this._publishedMonth,
-      "publishedYear": this._publishedYear,
-      "coverURL": this.coverURL,
-      "description": this.description,
-      "publisher": this.publisher
-    };
-  }
 
   /// Equality operator. Two books are equal if ISBN is the same.
   @override
