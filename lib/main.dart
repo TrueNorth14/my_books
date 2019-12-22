712de0a398c6ad2182d006553f85267431d7b3d3
TrueNorth14 <vishnu514.nair@gmail.com>
2019-12-21 23:43:43 -0500

    added network call, and test. Still need to implement exception handling for Http but books are retrieved just fine.

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,105 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_books/models/BookModel.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("api call test"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: RaisedButton(
+            child: Text("Call API test."),
+            onPressed: () => getBookModel("9780134689555"),
+          ),
+        ));
   }
 }
+
+/*
+   *  Asynchronous function that takes in an isbn string and
+   *  queries googles book api. Returns a BookModel.
+   * 
+   *  Google Books API documentation: https://developers.google.com/books/docs/v1/using
+   *  
+   */
+  Future<BookModel> getBookModel(String isbn) async {
+    //TODO: Catch http get exceptions
+
+    String url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn;
+    Map<String, String> requestHeaders = {
+      'Content-type': 'application/json',
+      'Accept': 'application/json'
+    };
+
+    BookModel myBook;
+    String title;
+    String author;
+    int publishedMonth;
+    int publishedDay;
+    int publishedYear;
+    String coverURL;
+    String description;
+    String publisher;
+
+    var response = await http.get(url, headers: requestHeaders);
+
+    //parse json reponse into dart objects
+    var jsonData = jsonDecode(response.body);
+
+    //remove unwanted items from jsonData and only get first from list
+    jsonData = jsonData["items"][0]["volumeInfo"];
+
+    title = jsonData["title"];
+    author =
+        jsonData["authors"].join(', '); //join all authors (seperate by comma)
+
+    //seperate date string into month, day, and year variables
+    //date format is YYYY-MM-DD
+    List<String> date = jsonData["publishedDate"].split('-');
+    publishedYear = int.parse(date[0]);
+    publishedMonth = int.parse(date[1]);
+    publishedDay = int.parse(date[2]);
+
+    //get cover image url from json
+    coverURL = jsonData["imageLinks"]["thumbnail"];
+
+    //description
+    description = jsonData["description"];
+
+    //publisher
+    publisher = jsonData["publisher"];
+
+    myBook = BookModel(int.parse(isbn), title, author, publishedMonth,
+        publishedDay, publishedYear, coverURL, description, publisher);
+
+    print(myBook.map);
+    return myBook;
+  }
