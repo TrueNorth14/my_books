cbb8a7f5b7bc07ec23d6da367b074761a5aefa90
TrueNorth14 <vishnu514.nair@gmail.com>
2019-12-21 23:40:02 -0500

    formatting no changes

--- lib/models/BookShelfModel.dart
+++ lib/models/BookShelfModel.dart
@@ -1,61 +1,58 @@
-import 'package:my_books/models/BookModel.dart';
+import 'BookModel.dart';
 
 class BookShelfModel extends Iterable<BookModel> {
+  /// The list that keeps track of all the books
+  List<BookModel> _books = [];
 
-    /// The list that keeps track of all the books
-    List<BookModel> _books = [];
+  @override
+  Iterator<BookModel> get iterator => new _BookShelfIterator(_books);
 
-    @override
-    Iterator<BookModel> get iterator => new _BookShelfIterator(_books);
-
-    /// Method to find book using isbn
-    BookModel findBook(int isbn) {
-        for (BookModel book in _books) {
-            if (book.isbn == isbn) return book;
-        }
-        return null;
+  /// Method to find book using isbn
+  BookModel findBook(int isbn) {
+    for (BookModel book in _books) {
+      if (book.isbn == isbn) return book;
     }
+    return null;
+  }
 
-    /// Method to add book to list
-    void addBook(BookModel book) => _books.add(book);
+  /// Method to add book to list
+  void addBook(BookModel book) => _books.add(book);
 
-    /// Method to remove book from list
-    void removeBook(int isbn) {
-        _books.remove(findBook(isbn));
-    }
+  /// Method to remove book from list
+  void removeBook(int isbn) {
+    _books.remove(findBook(isbn));
+  }
 
-    /* 
+  /* 
        * Dummy method for sorting the books by author.
     */
-    void sortByAuthor() {
-        // TODO: IMPLEMENT
-    }
+  void sortByAuthor() {
+    // TODO: IMPLEMENT
+  }
 
-    /* 
+  /* 
        * Dummy method for sorting the books by title.
     */
-    void sortByTitle() {
-        // TODO: IMPLEMENT
-    }
-
+  void sortByTitle() {
+    // TODO: IMPLEMENT
+  }
 }
 
 class _BookShelfIterator extends Iterator<BookModel> {
-    /// Just a regular schmegular Iterator, why no nested classes (?)
-    List<BookModel> _books = [];
-    int _currIndex = 0;
+  /// Just a regular schmegular Iterator, why no nested classes (?)
+  List<BookModel> _books = [];
+  int _currIndex = 0;
 
-    /// Default constructor, must pass in the original _books list
-    _BookShelfIterator(List<BookModel> _books) {
-        this._books = _books;
-    }
+  /// Default constructor, must pass in the original _books list
+  _BookShelfIterator(List<BookModel> _books) {
+    this._books = _books;
+  }
 
-    @override
-    BookModel get current => _books[_currIndex];
+  @override
+  BookModel get current => _books[_currIndex];
 
-    @override
-    bool moveNext() {
-        return  ++_currIndex < _books.length;
-    }
-    
+  @override
+  bool moveNext() {
+    return ++_currIndex < _books.length;
+  }
 }
