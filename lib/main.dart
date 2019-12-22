07cec2bb24bdb8c5031b8c58b9a8644b583c1a90
Jungwoo <jwoooh0719@gmail.com>
2019-12-22 16:56:04 -0500

    clean up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:my_books/models/BookModel.dart';
 
 void main() => runApp(MyApp());
 
