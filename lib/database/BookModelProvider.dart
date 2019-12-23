9e02bb3989ea3aa732e7cba9561d3bfc3884559d
Jungwoo <jwoooh0719@gmail.com>
2019-12-23 09:29:13 -0500

    Add method to query bookmodels in BookModelProvider

--- lib/database/BookModelProvider.dart
+++ lib/database/BookModelProvider.dart
@@ -7,44 +7,58 @@ import 'package:sqflite/sqflite.dart';
 import '../models/BookModel.dart';
 
 class BookModelProvider {
-  static const String DB_NAME = "bookModel.db";
+  static const String DB_NAME = "book_database.db";
   Database db;
 
   Future open() async {
     var databasesPath = await getDatabasesPath();
-    var path = join(databasesPath, "book");
+    var path = join(databasesPath, "$DB_NAME");
     db = await openDatabase(path, version: 1,
               onCreate: (Database db, int version) async {
                 await db.execute(
-                  "CREATE TABLE Book (isbn INTEGER PRIMARY KEY, title TEXT, author TEXT," +
+                  "CREATE TABLE books (isbn INTEGER PRIMARY KEY, title TEXT, author TEXT," +
                   "coverURL TEXT, description TEXT, publisher TEXT");
               });
   }
 
+  Future<List<BookModel>> retrieveBookModels() async {
+    final List<Map<String, dynamic>> maps = await db.query('books');
+    return List.generate(maps.length, (i) {
+      return BookModel(
+          maps[i]["isbn"],
+          maps[i]["title"],
+          maps[i]["author"],
+          maps[i]["coverURL"],
+          maps[i]["description"],
+          maps[i]["publisher"],
+      );
+    });
+  }
+
   Future<BookModel> insert(BookModel book) async {
     var database = db;
-    database.insert("book", book.toMap());
+    database.insert("books", book.toMap());
     return book;
   }
 
 
   Future<int> delete(int isbn) async {
     var database = db;
-    return await database.delete("book",
+    return await database.delete("books",
                    where: "isbn = ?",
                    whereArgs: [isbn]);
   }
 
   Future<int> update(BookModel book) async {
     var database = db;
-    return await database.update("book", book.toMap(),
+    return await database.update("books", book.toMap(),
                   where: "isbn = ?",
                   whereArgs: [book.isbn]);
   }
 
   Future<BookModel> getBookModel(int isbn) async {
     var database = db;
-    List<Map> maps = await database.query("book",
+    List<Map> maps = await database.query("books",
                 columns: ["isbn","title","author","coverURL","description","publisher"],
                 where: "isbn = ?",
                 whereArgs: [isbn]);
